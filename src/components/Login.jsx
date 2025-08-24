73344793d8dec07229f2ceeaa84bf9db5ca23783
HemavathyLakisha <hemaluki@gmail.com>
2025-08-24 21:17:11 +0530

    Login page created

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const[username, setUsername] = useState("");
+    const[password,setPassword] = useState("");
+    const[message,setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!username && !password){
+            setMessage("Both fields are Required")
+            return
+        }
+
+        if(username==="user" && password==="password"){
+            setMessage("Welcome, user!")
+        }else{
+            setMessage("Invalid username or password")
+        }
+
+    }
+    
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+        <label>
+            Username :
+            <input type="text" value={username} 
+            name="username" 
+            onChange={(e)=> setUsername(e.target.value)}
+            required
+            />
+        </label>
+        </div>
+        <div>
+         <label>
+            Password :
+            <input type="password" value={password} 
+            name="password"
+            onChange={(e)=>setPassword(e.target.value)}
+            required
+            />
+        </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
+
+export default Login;
